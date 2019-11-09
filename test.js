e4a5ea5d9cbdf51e9154557155eaded80420a9cf
zhangyongwang <20225614@qq.com>
2019-11-09 13:53:45 +0800

    ajax请求

---
+++ test.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+// 读 数据库
+const usersString = fs.readFileSync('./db/users.json').toString();
+const usersArray = JSON.parse(usersString);
+
+// 写 数据库
+const user3 = {"id": 3, "name": "tom", "password": "yyy"};
+usersArray.push(user3);
+const string = JSON.stringify(usersArray);
+fs.writeFileSync('./db/users.json', string);
+
